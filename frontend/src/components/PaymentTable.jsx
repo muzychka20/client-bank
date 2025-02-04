fe94a16e24eeb44aa84561fce828a9d6dcb2d3d5
KyryloMuzychka <kyryll2016@gmail.com>
2025-02-04 21:17:03 +0200

    feat: sort table

--- frontend/src/components/PaymentTable.jsx
+++ frontend/src/components/PaymentTable.jsx
@@ -2,19 +2,44 @@ import React, { useEffect, useState } from "react";
 import "../styles/PaymentTable.css";
 import { usePayments } from "../contexts/PaymentsContext";
 import LoadingIndicator from "./LoadingIndicator";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSortUp,
+  faSortDown,
+  faSort,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function PaymentTable({ loading, setLoading }) {
+  const [sortKey, setSortKey] = useState(null);
+  const [sortOrder, setSortOrder] = useState("asc");
+
   const { payments } = usePayments();
 
-  const keys = [
-    "Date",
-    "№ Doc",
-    "Summa",
-    "Status",
-    "NaznP",
-    "Client Name",
-    "Address",
-  ];
+  const handleSort = (key) => {
+    if (sortKey === key) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortKey(key);
+      setSortOrder("asc");
+    }
+  };
+
+  const sortedPayments = [...payments].sort((a, b) => {
+    if (!sortKey) return 0;
+
+    const aValue = a[sortKey];
+    const bValue = b[sortKey];
+
+    if (typeof aValue === "number") {
+      return sortOrder === "asc" ? aValue - bValue : bValue - aValue;
+    } else {
+      return sortOrder === "asc"
+        ? String(aValue).localeCompare(String(bValue))
+        : String(bValue).localeCompare(String(aValue));
+    }
+  });
+
+  const keys = ["Date", "№ Doc", "Summa", "Status", "NaznP", "Name", "Address"];
 
   const status = {
     1: "в обработке",
@@ -28,44 +53,45 @@ export default function PaymentTable({ loading, setLoading }) {
     5: "status-success",
   };
 
+  const titles = [
+    { label: "Date", key: "date" },
+    { label: "№ Doc", key: "num_doc" },
+    { label: "Summa", key: "sum" },
+    { label: "Status", key: "status" },
+    { label: "NaznP", key: "n_p" },
+    { label: "Name", key: "client_name" },
+    { label: "Address", key: "address" },
+  ];
+
   return (
     <>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg payment-table-block">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              {keys.map((key) => (
+              {titles.map((title) => (
                 <th
-                  key={key}
+                  key={title.key}
                   scope="col"
                   className="px-6 py-3 table-header-cell"
+                  onClick={() => handleSort(title.key)}
                 >
-                  {key}
+                  <a href="#" className={"table-header-cell-sort"}>
+                    {title.label}
+                    {title.key === sortKey ? (
+                      <FontAwesomeIcon
+                        icon={sortOrder === "asc" ? faSortUp : faSortDown}
+                      />
+                    ) : (
+                      <FontAwesomeIcon icon={faSort} />
+                    )}
+                  </a>
                 </th>
               ))}
             </tr>
           </thead>
-          {payments.length > 0 ? (
-            <tfoot>
-              <tr class="font-semibold text-gray-900 dark:text-white">
-                <th scope="row" class="px-6 py-4 text-base table-cell">
-                  Total
-                </th>
-                <td class="px-6 py-4 table-cell">3 test</td>
-                <td class="px-6 py-4 table-cell">21.000 test</td>
-                <td class="px-6 py-4 table-cell"></td>
-                <td class="px-6 py-4 table-cell"></td>
-                <td class="px-6 py-4 table-cell"></td>
-                <td class="px-6 py-4 table-cell"></td>
-              </tr>
-            </tfoot>
-          ) : (
-            <tfoot></tfoot>
-          )}
-        </table>
-        {payments.length > 0 ? (
           <tbody>
-            {payments.map((payment) => (
+            {sortedPayments.map((payment) => (
               <tr
                 key={payment.num_doc}
                 className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
@@ -84,11 +110,27 @@ export default function PaymentTable({ loading, setLoading }) {
               </tr>
             ))}
           </tbody>
-        ) : (
-          <div className="table-loading relative overflow-x-auto shadow-md sm:rounded-lg payment-table-block">
-            {loading && <LoadingIndicator />}
-          </div>
-        )}
+          {payments.length > 0 ? (
+            <tfoot>
+              <tr className="font-semibold text-gray-900 dark:text-white">
+                <th scope="row" className="px-6 py-4 text-base table-cell">
+                  Total
+                </th>
+                <td className="px-6 py-4 table-cell">{0}</td>
+                <td className="px-6 py-4 table-cell">{0}</td>
+                <td className="px-6 py-4 table-cell"></td>
+                <td className="px-6 py-4 table-cell"></td>
+                <td className="px-6 py-4 table-cell"></td>
+                <td className="px-6 py-4 table-cell"></td>
+              </tr>
+            </tfoot>
+          ) : (
+            <tfoot></tfoot>
+          )}
+        </table>
+        <div className="table-loading relative overflow-x-auto shadow-md sm:rounded-lg payment-table-block">
+          {loading && <LoadingIndicator />}
+        </div>
       </div>
     </>
   );
